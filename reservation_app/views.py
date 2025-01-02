291c836c0874525e23647aa95005f13770c9b62f
Roshan Sequeira <roshan@secureworkspace.tech>
2025-01-02 17:56:47 +0000

    fresh new package install and configure new project and app

---
+++ reservation_app/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+# Create your views here.
+def myreservation (request):
+    return HttpResponse ("Welcome to Roshan's reservation system")
+
