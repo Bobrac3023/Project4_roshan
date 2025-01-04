b786ced25a8a19d43e1ffcda85482965cd459ce3
Roshan Sequeira <roshan@secureworkspace.tech>
2025-01-04 16:48:52 +0000

    used renamed Reservation model to update the database

--- reservation_app/admin.py
+++ reservation_app/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
-from .models import reservation
+from .models import Reservation
+
 
 # Register your models here
-admin.site.register(reservation)
+admin.site.register(Reservation)
+
+
