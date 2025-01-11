254b669e1a4472ce1d67d788d8afd62a7a3f0011
Roshan Sequeria <roshan@secureworkspace.tech>
2025-01-11 09:15:37 +0000

    configure  incoming POST request from our form in views.py

--- reservation_app/views.py
+++ reservation_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.views import generic
+from django.contrib import messages
 from .models import Reservation
 from .forms import ReservationForm
 
@@ -14,21 +15,28 @@ class PostList(generic.ListView):
 
 
 def about_me(request):
-        """
+
+    """
         Renders the index.html page
         """
-        about = Reservation.objects.all()
-        #about = Reservation.objects.all().order_by('-updated_on').first()
-        reservation_form = ReservationForm()
+    if request.method == "POST":
+        reservation_form = ReservationForm(data=request.POST)
+        if reservation_form.is_valid():
+            reservation_form.save()
+            messages.add_message(request, messages.SUCCESS, "Reservation request received. Please check your inbox for an confirmation email")
+        
+    about = Reservation.objects.all()
+    #about = Reservation.objects.all().order_by('-updated_on').first()
+    reservation_form = ReservationForm()
 
-        return render(
-            request,
-            "reservation/index.html",
+    return render(
+        request,
+        "reservation/index.html",
             {
-                """
-                adding reservation an reservation form variables to the context in the render helper function
-                """
+            """
+            adding reservation an reservation form variables to the context in the render helper function
+            """
             "about": about,
             "reservation_form": reservation_form
             },
-        )
+    )
