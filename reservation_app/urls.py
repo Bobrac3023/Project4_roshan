53f9bc6f0d8d4e87ad3b006d5884f3712de8dd5a
Roshan Sequeria <roshan@secureworkspace.tech>
2025-01-11 07:57:23 +0000

    modify urls for  reservationform and render on index.html

--- reservation_app/urls.py
+++ reservation_app/urls.py
@@ -2,7 +2,7 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    #path('', views.PostList.as_view, name='index.html'),
-    #path('', views.PostList.as_view(), name='about.html'),
     path('', views.about_me, name='index.html'),
-]
+    path('', views.PostList.as_view(), name='index.html'),
+    path('', views.PostList.as_view(), name='about.html'),
+    ]
--- reservation_app/views.py
+++ reservation_app/views.py
@@ -1,15 +1,15 @@
 from django.shortcuts import render
-#from django.views import generic
+from django.views import generic
 from .models import Reservation
 from .forms import ReservationForm
 
 # Create your views here.
 
-#class PostList(generic.ListView):
-#    model = Reservation
-#    queryset = Reservation.objects.all()
-#    template_name="reservation/index.html"
-#    #paginate_by = 6
+class PostList(generic.ListView):
+    model = Reservation
+    queryset = Reservation.objects.all()
+    template_name="reservation/index.html"
+    #paginate_by = 6
 
 
 
