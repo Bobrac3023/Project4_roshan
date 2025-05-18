b1b660c914354debfb6c2681fdb0fd8d9099cdaf
Bobrac3023 <roshan@secureworkspace.tech>
2025-05-18 22:39:57 +0400

    changes to urls.py

--- reservation_app/urls.py
+++ reservation_app/urls.py
@@ -1,3 +1,4 @@
+"""
 from . import views
 from django.urls import path
 
@@ -12,3 +13,13 @@ urlpatterns = [
          name='confirm_delete'),
 
     ]
+"""
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.PostList.as_view(), name='index'),  # Homepage
+    path('about/', views.about_me, name='about'),      # About page
+    path('reservation/update/<int:pk>/', views.update_reservation, name='update_reservation'),  # Update reservation
+    path('reservation/confirm_delete/<int:pk>/', views.delete_reservation, name='confirm_delete'),  # Delete reservation
+]
