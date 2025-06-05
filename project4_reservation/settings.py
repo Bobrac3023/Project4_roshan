c6521c2408eb6d18938a7c9ea11f89b0315ff301
Bobrac3023 <roshan@secureworkspace.tech>
2025-06-05 10:38:47 +0400

    change allowed hosts section in settings.py

--- project4_reservation/settings.py
+++ project4_reservation/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com"]
+ALLOWED_HOSTS = ["127.0.0.1", ".herokuapp.com" , "https://*.codeinstitute-ide.net/",]
 
 # Application definition
 INSTALLED_APPS = [
